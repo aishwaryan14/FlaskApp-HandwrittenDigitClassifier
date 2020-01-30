a2f6b6c749219fc2e2383c538790b47435175b69
aishwaryan14 <44488855+aishwaryan14@users.noreply.github.com>
2020-01-30 21:23:40 +0530

    update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app=Flask(__name__)
 app.secret_key="secret"
 @app.route("/",methods=['GET','POST'])
 
+			
 def index():
 	try:
 		prediction = session["prediction"]
@@ -20,22 +21,25 @@ def index():
 		return render_template('index.html',prediction="",accuracy="")
 	if request.method=='POST':
 		if request.form["action"]=="SUBMIT":
-			pred=request.form["pic"]
-			img = Image.open("static/"+pred)
-			img1 = img.resize((8,8))
-			img1= img1.convert('L')
-			img1 = ImageOps.invert(img1)
-			temp=np.asarray(img1)/255
-			temp=temp.flatten()
-			loaded_model = pickle.load(open("model.pkl","rb"))
-			result = loaded_model.predict([temp])
+			pred=request.files["pic"]
+			img = Image.open(pred)
+			img1,temp,result,accuracy = getWeights(img)
 			print(result[0])
-			accuracy = round(max(loaded_model.predict_proba([temp])[0])*100,2)
-			plt.matshow(img1)
+			#plt.matshow(img1)
 			#plt.imshow(img1)
 			#plt.savefig('static/invimg.png')
-			plt.show()
+			#plt.show()
 			return render_template('index.html',prediction=result[0],predict="Prediction:",accuracy=accuracy,Accuracy="Accuracy:")
+def getWeights(img):
+	img1 = img.resize((8,8))
+	img1= img1.convert('L')
+	img1 = ImageOps.invert(img1)
+	temp=np.asarray(img1)/255
+	temp=temp.flatten()
+	loaded_model = pickle.load(open("model.pkl","rb"))
+	result = loaded_model.predict([temp])
+	accuracy = round(max(loaded_model.predict_proba([temp])[0])*100,2)
+	return img1,temp,result,accuracy
 
 if __name__ == '__main__':
 	app.run(host='localhost', port=1098, debug=True)
